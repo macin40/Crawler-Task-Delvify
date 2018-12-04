f5d4c8ea5892dbb75feeaab41bb028509a6c805b
Abhishek Mani <38850316+abhishek-mani@users.noreply.github.com>
2018-12-04 15:26:53 +0530

    created file scrapper.js

--- routes.js
+++ routes.js
@@ -4,132 +4,24 @@ const request = require('request');
 const cheerio = require('cheerio');
 const tabletojson = require('tabletojson');
 const he = require('he');
-const googleTranslate = require('google-translate')(process.env.GOOGLE_API_KEY);
+const googleTranslate = require('google-translate')('AIzaSyB9U9dxk1xveVSQC2FjMxxlobJZrUsON_o');
 const querystring = require('querystring');
 const fs = require('fs');
 const path = require('path');
 module.exports = router;
+const file = fs.createWriteStream('./public/result.json', {flags: 'w'});
 
-router.get("/", (req, res) => {
-
-    console.log("********** started ***************");
-
-    const url = 'http://localhost:4000/test.html';
-
-    request(url, function (error, response, html) {
-
-        let localeKey = [], localeValue = [];
-        /* Product detail parsing*/
-        let $ = cheerio.load(html);
-        let productDetail = $(".tbl_type07").html();
-        productDetail = productDetail.substring(productDetail.indexOf('<tbody>'), productDetail.indexOf('</tbody>') + 1);
-        productDetail = `<table>${productDetail}</table>`;
-
-        const resultObj = {};
-        let converted = tabletojson.convert(productDetail, {forceIndexAsNumber: true, useFirstRowForHeadings: true});
-        converted = converted[0];
-
-        let results = {};
-        for (let index = 0; index < converted.length; index++) {
-
-            results[converted[index]["0"]] = converted[index]["1"];
-
-            localeKey.push(converted[index]["0"]);
-            localeValue.push(converted[index]["1"]);
-        }
-
-        /****************************/
-
-        let parent = $('.group_1').html().toString();
-        const parsedJsonObj = {};
-        let start = parent.indexOf('<dl>');
-        let end = parent.indexOf('</dl>');
-        let str = parent.substring(start + 4, end);
-
-        start = str.indexOf('<dt>');
-        end = str.indexOf('</dt>');
-        let key = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').toString());
-
-        start = str.indexOf('<dd>');
-        end = str.indexOf('</dd>');
-        let value = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
-
-        resultObj.Price = value;
-
-        localeKey.push("Price");
-        localeValue.push(value);
-
-        parent = $('.sp.salecoupon.couponDown').text().replace(/\t/g, ' ').replace(/\n/g, ' ').trim();
-
-        resultObj.Discount_percent = parent;
-
-        localeKey.push("Discount_percent");
-        localeValue.push(parent);
-
-
-        resultObj.Manufacture = results["제조자"];
-
-        localeKey.push("Manufacture");
-        localeValue.push(results["제조자"]);
-        /* Shipping Fee parsing*/
-
-        parent = $('.deli').html().toString();
-
-        start = parent.indexOf('<dd>');
-        end = parent.indexOf('</dd>');
-
-        value = he.decode(parent.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
-
-        resultObj.Shipping_fee = value;
-        localeKey.push("Shipping_fee");
-        localeValue.push(value);
-
-        resultObj.product_details = results;
-
-        /**************************/
-
-        /* Parsing of image url's**/
-
-        parent = $('#mainGoodsImage').attr('src');
-        parent = "http:" + parent;
-        parsedJsonObj.image_urls = parent;
-
-
-        /*************************/
-
-        /* Localisation of Korean to English */
-        Promise.all([translate(localeKey), translate(localeValue)]).then((data) => {
-            for (let index = 0; index < data[0].length; index++) {
-                const parsedKey = data[0][index].translatedText.split(' ').join('_');
-                parsedJsonObj[parsedKey] = data[1][index].translatedText;
-            }
-            /* parsing final o/p */
-            const details = {}, Product_details = {};
-            for (let key in parsedJsonObj) {
-                if (key !== "Price" && key !== "Discount_percent" && key !== "Manufacture" && key !== "Shipping_fee" && key !== "image_urls") {
-                    Product_details[key] = parsedJsonObj[key];
-                }
-                else {
-                    details[key] = parsedJsonObj[key];
-                }
-            }
-            details.Product_details = Product_details;
-            /**************************/
-            res.send(details)
-        })
-
-        /**************************/
-
-    });
-
-});
-
-router.get("/product", (req, res) => {
+try {
+    console.log("Old File Deleted!!")
+} catch (e) {
+    console.log("No file exists!");
+}
+router.getTopProducts = (req, res) => {
     let anyData;
     const url = 'http://www.akmall.com/bestseller/BestShopAkmall.do';
 
     let arr = [];
-    request(url, function (error, response, html) {
+    request(url, async function (error, response, html) {
         global.cook = response.headers['set-cookie'];
         let $ = cheerio.load(html);
         const finalResponse = [];
@@ -148,6 +40,7 @@ router.get("/product", (req, res) => {
                 let url = elem('a').attr('onclick');
                 url = url.substring(url.indexOf('(') + 3, url.lastIndexOf(')')).replace(/'/g, '');
                 url = url.split(',');
+
                 arr.push({
                     url: 'http://www.akmall.com/' + url[0] + '?ctgId=' + url[4].trim(),
                     groupId: url[2].trim(),
@@ -160,50 +53,33 @@ router.get("/product", (req, res) => {
                 convertToJSON(obj, (data) => {
                     if (data) {
                         resolve(data);
-                    }
-                    else
+                    } else
                         reject(data);
                 })
             })
         });
-        let resp = "";
-        Promise.all(requestArray).then((result) => {
-            // for (let index = 0; index < result.length; index++) {
-            //     resp += result[index];
-            // }
-            try {
-                fs.unlinkSync('./public/result.json');
-                console.log("Old File Deleted!!")
-            } catch (e) {
-                console.log("No file exists!");
-            } finally {
-                fs.appendFileSync('./public/result.json', JSON.stringify(result));
-                // res.end('<a href="'+ req.protocol + '://' + req.get('host') + '/public/result.json">'+ 'File Url</a>')
-                res.download((path.join(__dirname, 'public/result.json')));
-
-            }
-
-
-        });
-
+        Promise.all(requestArray).then((data) => {
+            console.log("done");
+        })
     });
-
-
-});
+};
 
 function translate(localeDataArray) {
     return new Promise((resolve, reject) => {
-        googleTranslate.translate(localeDataArray, 'ko', 'en', function (err, translations) {
-            if (err)
-                reject(err);
-            resolve(translations)
-        });
+        // googleTranslate.translate(localeDataArray, 'ko', 'en', function (err, translations) {
+        //     if (err)
+        //         reject(err);
+        //     resolve(translations)
+        // });
+        resolve(localeDataArray);
     });
 }
 
 
 function convertToJSON(obj, cb) {
-    console.log(obj);
+
+    const productListArray = [];
+    // console.log(obj);
     const postData = {
         price_min: 100,
         price_max: 1000000,
@@ -219,7 +95,7 @@ function convertToJSON(obj, cb) {
         ranking: 1,
         sexCode: "all",
     };
-    console.log(postData);
+    // console.log(postData);
     request({
         method: 'POST',
         url: obj.url,
@@ -228,7 +104,7 @@ function convertToJSON(obj, cb) {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         form: postData
-    }, function (error, response, html) {
+    }, async function (error, response, html) {
         let $ = cheerio.load(html);
 
         let str = [];
@@ -238,78 +114,137 @@ function convertToJSON(obj, cb) {
             const resultObj = {};
 
             let elementHtml = cheerio.load(element);
-            const imageUrl = elementHtml('div.thumb').find('img').first().attr('src');
+            let productUrl = elementHtml('div.thumb').find('a').first().attr('onclick');
 
-            resultObj.imageUrl = 'http:' + imageUrl;
-            console.log(resultObj.imageUrl);
-            /* *************** Scrapping of the element description and discount*****************/
-            elementHtml('dl.cont').find('dt a').each((index1, element1) => {
+            if (productUrl) {
+                productUrl = productUrl.substring((productUrl.indexOf('(') + 1), productUrl.lastIndexOf(')')).replace(/'/g, '');
+                productUrl = productUrl.split(',');
+                productListArray.push('http://www.akmall.com/goods/GoodsDetail.do?goods_id=' + productUrl[0] + '&urlpath=' + productUrl[1].trim() + productUrl[2].trim());
+            }
 
-                const el = cheerio.load(element1);
-                resultObj.prom = el('.prom').html() ? he.decode(el('.prom').html().toString().replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ')) : '';
-                el('.prom').length = 0;
-                resultObj.prom2 = el('.prom2').html() ? he.decode(el('.prom2').html().toString().replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ')) : '';
-                el('.prom2').length = 0;
-
-                resultObj.description = el('span').html() ? he.decode(el('span').html().toString()) : '';
+        });
+        console.log(productListArray.length);
+        if (productListArray.length) {
+            const requestProductArray = productListArray.map((url) => {
+                return new Promise((resolve, reject) => {
+                    translateProduct(url, function (data) {
+                        if (data)
+                            resolve(data)
+                        else
+                            reject(data);
+                    });
+                })
             });
-            /*********************************/
-
-            /* *************** Scrapping of the element price*****************/
-            elementHtml('dl.cont').find('dd.price').each((index1, element1) => {
-
-                const el = cheerio.load(element1);
-
-                resultObj.price = el('span.price0').html() ? he.decode(el('span.price0').html().toString().replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ')) : '';
-                resultObj.price = resultObj.price.substring(resultObj.price.indexOf(':') + 1).trim();
-
-                resultObj.discount = el('span.price2').html() ? he.decode(el('span.price2').html().toString().replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ')) : '';
-                resultObj.discount = resultObj.discount.substring(resultObj.discount.indexOf(':') + 1).trim();
-
-
+            Promise.all(requestProductArray).then((data) => {
+                cb(true);
+            }).catch((err) => {
+                console.log(err)
             });
-            /*********************************/
+        }
 
 
-            /* *************** Scrapping of the element price*****************/
-            elementHtml('dl.cont').find('dd.option').children('span').each((index1, element1) => {
+    });
+}
 
-                const el = cheerio.load(element1);
+function translateProduct(url, cb) {
 
-                if (el.html()) {
+    request(url, function (error, response, html) {
 
-                    let data;
-                    data = he.decode(el.html().toString().replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' '));
+        console.log(url)
+        let localeKey = [], localeValue = [];
+        if (html) {
+            let $ = cheerio.load(html);
+            let productDetail = $(".tbl_type07").html();
+            if (productDetail) {
+                productDetail = productDetail.substring(productDetail.indexOf('<tbody>'), productDetail.indexOf('</tbody>') + 1);
+                productDetail = `<table>${productDetail}</table>`;
 
-                    data = data.split(' ');
-                    data = data.filter(elem => elem.length > 0);
+                const resultObj = {};
+                let converted = tabletojson.convert(productDetail, {
+                    forceIndexAsNumber: true,
+                    useFirstRowForHeadings: true
+                });
+                converted = converted[0];
 
-                    if (el.html().toString().indexOf('ico_coupon') > -1) {
-                        resultObj.coupon = data.length === 2 ? data[1] : '';
-                    }
-                    else if (el.html().toString().indexOf('ico_discount') > -1) {
-                        resultObj.instantDiscount = data.length === 2 ? data[1] : '';
-                    }
-                    else if (el.html().toString().indexOf('ico_card') > -1) {
-                        resultObj.NHcard = data.length === 2 ? data[1] : '';
-                    }
-                    else if (el.html().toString().indexOf('ico_inter_free') > -1) {
-                        resultObj.interest = data.length === 2 ? data[1] : '';
-                    }
-                    else if (el.html().toString().indexOf('ico_review') > -1) {
-                        resultObj.review = data.length === 2 ? data[1] : '';
-                    }
-                    else if (el.html().toString().indexOf('ico_free_ship') > -1) {
-                        resultObj.shipment = data.length === 2 ? data[1] : '';
+                let results = {};
+                if (converted) {
+                    for (let index = 0; index < converted.length; index++) {
+
+                        results[converted[index]["0"]] = converted[index]["1"];
+
+                        localeKey.push(converted[index]["0"]);
+                        localeValue.push(converted[index]["1"]);
                     }
                 }
-            });
-            /*********************************/
 
-            /* Pushing final object to the array to display response **/
+                let parent = $('.group_1').html().toString();
+                const parsedJsonObj = {};
+                let start = parent.indexOf('<dl>');
+                let end = parent.indexOf('</dl>');
+                let str = parent.substring(start + 4, end);
 
-            finalResponse.push(resultObj);
-        });
-        cb(finalResponse);
-    });
+                start = str.indexOf('<dt>');
+                end = str.indexOf('</dt>');
+                let key = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').toString());
+
+                start = str.indexOf('<dd>');
+                end = str.indexOf('</dd>');
+                let value = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
+
+                resultObj["Price"] = value;
+
+                localeKey.push("Price");
+                localeValue.push(value);
+
+                parent = $('.sp.salecoupon.couponDown').text().replace(/\t/g, ' ').replace(/\n/g, ' ').trim();
+
+                resultObj["Discount_percent"] = parent;
+
+                localeKey.push("Discount_percent");
+                localeValue.push(parent);
+
+
+                resultObj["Manufacture"] = results["제조자"];
+
+                localeKey.push("Manufacture");
+                localeValue.push(results["제조자"]);
+                /* Shipping Fee parsing*/
+
+                parent = $('.deli').html().toString();
+
+                start = parent.indexOf('<dd>');
+                end = parent.indexOf('</dd>');
+
+                value = he.decode(parent.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
+
+                resultObj["Shipping_fee"] = value;
+                localeKey.push("Shipping_fee");
+                localeValue.push(value);
+
+                resultObj.product_details = results;
+
+                /**************************/
+
+                /* Parsing of image url's**/
+
+                parent = $('#mainGoodsImage').attr('src');
+                parent = "http:" + parent;
+                parsedJsonObj.image_urls = parent;
+                // file.write(JSON.stringify(resultObj));
+                console.log("Written");
+                cb(true)
+            }
+        }
+    })
+}
+
+function mapPromiseToMakeSync(obj) {
+    return new Promise((resolve, reject) => {
+        convertToJSON(obj, (data) => {
+            if (data) {
+                resolve(data);
+            } else
+                reject("");
+        })
+    })
 }
---
+++ scrapper.js
@@ -0,0 +1,254 @@
+const express = require("express");
+const router = express.Router();
+const request = require('request');
+const cheerio = require('cheerio');
+const tabletojson = require('tabletojson');
+const he = require('he');
+const googleTranslate = require('google-translate')('AIzaSyB9U9dxk1xveVSQC2FjMxxlobJZrUsON_o');
+const querystring = require('querystring');
+const fs = require('fs');
+const path = require('path');
+module.exports = router;
+
+const file = fs.createWriteStream('./public/result.json', {flags: 'w'});
+
+try {
+    console.log("Old File Deleted!!")
+} catch (e) {
+    console.log("No file exists!");
+}
+router.getTopProducts = (req, res) => {
+
+    const url = 'http://www.akmall.com/bestseller/BestShopAkmall.do';
+    let arr = [];
+    request(url, async function (error, response, html) {
+        global.cook = response.headers['set-cookie'];
+        let $ = cheerio.load(html);
+
+        let elemLength = 0;
+        $('ul.tree').find('li').each((index, element) => {
+            let el = cheerio.load(element);
+            el('ul').find('li').each((index1, element1) => {
+                elemLength++;
+            });
+        });
+        $ = cheerio.load(html);
+        $('ul.tree').find('li').each((index, element) => {
+            let el = cheerio.load(element);
+            el('ul').find('li').each((index1, element1) => {
+                let elem = cheerio.load(element1);
+                let url = elem('a').attr('onclick');
+                url = url.substring(url.indexOf('(') + 3, url.lastIndexOf(')')).replace(/'/g, '');
+                url = url.split(',');
+
+                arr.push({
+                    url: 'http://www.akmall.com/' + url[0] + '?ctgId=' + url[4].trim(),
+                    groupId: url[2].trim(),
+                    ctgId: url[4].trim()
+                });
+            });
+        });
+        let urlList = [];
+        const requestArray = arr.map((obj) => {
+            return new Promise((resolve, reject) => {
+                convertToJSON(obj, (data) => {
+                    if (data) {
+                        resolve(data);
+                    } else
+                        reject(data);
+                })
+            })
+        });
+        Promise.all(requestArray).then(async (data) => {
+            for (let index = 0; index < data.length; index++) {
+                urlList = urlList.concat(data[index]);
+            }
+            console.log("done");
+            for (let counter = 0; counter < urlList.length; counter++) {
+                const data = await translateProduct(urlList[counter]);
+                file.write(JSON.stringify(data));
+            }
+        });
+
+    });
+};
+
+function translate(localeDataArray) {
+    return new Promise((resolve, reject) => {
+        // googleTranslate.translate(localeDataArray, 'ko', 'en', function (err, translations) {
+        //     if (err)
+        //         reject(err);
+        //     resolve(translations)
+        // });
+        resolve(localeDataArray);
+    });
+}
+
+
+function convertToJSON(obj, cb) {
+
+    const productListArray = [];
+    // console.log(obj);
+    const postData = {
+        price_min: 100,
+        price_max: 1000000,
+        sex: "all",
+        age: "all",
+        groupId: obj.groupId,
+        rank_in_group: 1,
+        ctgId: obj.ctgId,
+        disp_div: "ctg",
+        sort_div: "popular",
+        mall_div: "AKMall",
+        pageIdx: 1,
+        ranking: 1,
+        sexCode: "all",
+    };
+
+    request({
+        method: 'POST',
+        url: obj.url,
+        headers: {
+            'Content-Length': postData.length,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: postData
+    }, async function (error, response, html) {
+        let $ = cheerio.load(html);
+
+        let str = [];
+        const finalResponse = [];
+
+        $('#tab').find('ul.prod_list > li').each((index, element) => {
+            const resultObj = {};
+
+            let elementHtml = cheerio.load(element);
+            let productUrl = elementHtml('div.thumb').find('a').first().attr('onclick');
+
+            if (productUrl) {
+                productUrl = productUrl.substring((productUrl.indexOf('(') + 1), productUrl.lastIndexOf(')')).replace(/'/g, '');
+                productUrl = productUrl.split(',');
+                productListArray.push('http://www.akmall.com/goods/GoodsDetail.do?goods_id=' + productUrl[0] + '&urlpath=' + productUrl[1].trim() + productUrl[2].trim());
+            }
+        });
+        console.log(productListArray.length);
+        cb(productListArray);
+    });
+}
+
+function translateProduct(url) {
+    return new Promise((resolve, reject) => {
+        fetchItem(url).then((data) => {
+            resolve(data)
+        }).then((err) => {
+            reject(err)
+            console.log(err)
+        });
+    })
+
+
+}
+
+function mapPromiseToMakeSync(obj) {
+    return new Promise((resolve, reject) => {
+        convertToJSON(obj, (data) => {
+            if (data) {
+                resolve(data);
+            } else
+                reject("");
+        })
+    })
+}
+
+function fetchItem(url) {
+    return new Promise((resolve, reject) => {
+        const resultObj = {};
+        console.log(url)
+        request(url, function (error, response, html) {
+            console.log(error)
+            let localeKey = [], localeValue = [];
+            let $ = cheerio.load(html);
+            let productDetail = $(".tbl_type07").html();
+            if (productDetail) {
+                productDetail = productDetail.substring(productDetail.indexOf('<tbody>'), productDetail.indexOf('</tbody>') + 1);
+                productDetail = `<table>${productDetail}</table>`;
+
+                let converted = tabletojson.convert(productDetail, {
+                    forceIndexAsNumber: true,
+                    useFirstRowForHeadings: true
+                });
+                converted = converted[0];
+
+                let results = {};
+                if (converted) {
+                    for (let index = 0; index < converted.length; index++) {
+
+                        results[converted[index]["0"]] = converted[index]["1"];
+
+                        localeKey.push(converted[index]["0"]);
+                        localeValue.push(converted[index]["1"]);
+                    }
+                }
+
+                let parent = $('.group_1').html().toString();
+                const parsedJsonObj = {};
+                let start = parent.indexOf('<dl>');
+                let end = parent.indexOf('</dl>');
+                let str = parent.substring(start + 4, end);
+
+                start = str.indexOf('<dt>');
+                end = str.indexOf('</dt>');
+                let key = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').toString());
+
+                start = str.indexOf('<dd>');
+                end = str.indexOf('</dd>');
+                let value = he.decode(str.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
+
+                resultObj["Price"] = value;
+
+                localeKey.push("Price");
+                localeValue.push(value);
+
+                parent = $('.sp.salecoupon.couponDown').text().replace(/\t/g, ' ').replace(/\n/g, ' ').trim();
+
+                resultObj["Discount_percent"] = parent;
+
+                localeKey.push("Discount_percent");
+                localeValue.push(parent);
+
+
+                resultObj["Manufacture"] = results["제조자"];
+
+                localeKey.push("Manufacture");
+                localeValue.push(results["제조자"]);
+                /* Shipping Fee parsing*/
+
+                parent = $('.deli').html().toString();
+
+                start = parent.indexOf('<dd>');
+                end = parent.indexOf('</dd>');
+
+                value = he.decode(parent.substring(start + 4, end).replace(/<[^>]+>/g, ' ').replace(/\t/g, ' ').replace(/\n/g, ' ').trim());
+
+                resultObj["Shipping_fee"] = value;
+                localeKey.push("Shipping_fee");
+                localeValue.push(value);
+
+                resultObj.product_details = results;
+
+                /**************************/
+
+                /* Parsing of image url's**/
+
+                parent = $('#mainGoodsImage').attr('src');
+                parent = "http:" + parent;
+                parsedJsonObj.image_urls = parent;
+                // file.write(JSON.stringify(resultObj));
+                // console.log("Written");
+                // console.log(resultObj)
+
+            }
+            resolve(resultObj);
+        });
+    })
+}
